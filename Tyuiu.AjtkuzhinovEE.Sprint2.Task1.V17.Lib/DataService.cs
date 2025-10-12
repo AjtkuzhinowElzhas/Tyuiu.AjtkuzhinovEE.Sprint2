dadb58c9d2c4791066c63fc6297c5b1355e38c42
AjtkuzhinovElzhas <aitkuzinow8@gmail.com>
2025-10-12 19:19:46 +0300

    Task1

---
+++ Tyuiu.AjtkuzhinovEE.Sprint2.Task1.V17.Lib/DataService.cs
@@ -0,0 +1,30 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+
+
+
+//Написать программу из операций сравнений
+//(==, !=, <, >, <=, >=, последовательность можно чередовать, но использовать один раз в выражении)
+//и логических операций (|, &, ||, &&, !, ^, последовательность операций не должна нарушаться)
+//, а также арифметических выражений
+//, которая вернет логическую последовательность(массив): (True, False, True, True, True, False)
+//, при a = 135, b = 123, c = 455, d = 321
+namespace Tyuiu.AjtkuzhinovEE.Sprint2.Task1.V17.Lib
+{
+    public class DataService : ISprint2Task1V17
+    {
+        public bool[] GetLogicOperations(int a, int b, int c, int d)
+        {
+            bool[] res = new bool[6];
+            res[0] = (a > b)|(c < d);      //(a > b)-true  (c < d)-false , поэтому вернём true
+            res[1] = (c > d) & (a < b);    //(c > b)-true  (a < b)-false , поэтому вернём false
+            res[2] = (a > b) ||(c < d);    //(a > b)-true  (c < d)-false , поэтому вернём true
+            res[3] = (c > d) &&( b <= a);  //(c > d)-true  (b <= a)-true , поэтому вернём true
+            res[4] = !(a < b);             //(a < b)-false               , поэтому вернём true
+            res[5] = (c > d) ^(b <= a);    //(c > d)-true  (b <= a)-true , поэтому вернём false
+
+
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.AjtkuzhinovEE.Sprint2.Task1.V17/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.AjtkuzhinovEE.Sprint2.Task1.V17.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        int a = 135;
+        int b = 123;
+        int c = 455;
+        int d = 321;
+
+        bool[] res = new bool[6];
+        res = ds.GetLogicOperations(a, b, c, d);
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("а = " + a);
+        Console.WriteLine("b = " + b);
+        Console.WriteLine("с = " + c);
+        Console.WriteLine("d = " + d);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        for (int i = 0; i < 6; i++)
+        {
+            Console.WriteLine(res[i]);
+        }
+
+        
+
+    }
+}
