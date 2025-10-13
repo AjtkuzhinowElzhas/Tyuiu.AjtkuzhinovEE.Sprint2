c2e4e3ae87f728fafc0e840a42587f18391ecf5a
AjtkuzhinovElzhas <aitkuzinow8@gmail.com>
2025-10-13 16:35:40 +0300

    Task2

---
+++ Tyuiu.AjtkuzhinovEE.Sprint2.Task2.V9.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿
+using Tyuiu.AjtkuzhinovEE.Sprint2.Task2.V9.Lib;
+namespace Tyuiu.AjtkuzhinovEE.Sprint2.Task2.V9.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCheckDotInShadedArea()
+        {
+         
+            DataService ds = new DataService();
+            int x = 4;
+            int y = 4;
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+
+            Assert.AreEqual(wait, res);
+
+
+
+        }
+    }
+}
